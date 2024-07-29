979aed5c5cbcc7ed27d81a54397f6f77d860b4a3
Jorgecardenas1 <jorgehernancardenas@gmail.com>
2024-07-29 11:30:19 -0400

    some change

--- PipeLine.py
+++ PipeLine.py
@@ -435,7 +435,7 @@ def main(args):
     if optimizing:
         pass
     else:
-        z=torch.rand(parser.latent)
+        z=torch.randn(parser.latent)
 
     input_tensor = prepare_data(device,spectra,values_array,z)
     
